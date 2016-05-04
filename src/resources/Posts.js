3f214abddec705111bb4cac493003d086fbbccde
Teresa Do <tdo@classy.org>
2016-05-04 12:46:41 -0700

    Posts resource added

---
+++ src/resources/Posts.js
@@ -0,0 +1,15 @@
+import Resource from '../ClassyResource';
+
+class Posts extends Resource {
+  constructor(Classy) {
+    super(Classy, {
+    	basic: ['retrieve', 'delete', 'update'],
+      creates: ['comments', 'likes'],
+      lists: ['comments', 'likes'],
+      path: '/posts'
+    });
+
+  }
+}
+
+export default Posts;

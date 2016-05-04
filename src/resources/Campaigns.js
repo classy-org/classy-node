8506ca186c5348e5b386780271f6eb4ad0b7a51b
Teresa Do <tdo@classy.org>
2016-05-04 13:04:10 -0700

    added list:posts to campaign resource

--- src/resources/Campaigns.js
+++ src/resources/Campaigns.js
@@ -14,6 +14,7 @@ class Campaigns extends Resource {
         'fundraising-pages',
         'fundraising-teams',
         'messages',
+        'posts',
         'questions',
         'recurring-donation-plans',
         'transactions',

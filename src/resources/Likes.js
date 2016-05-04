ce597ba7fd75c59dea4572859e92fe22c69627c9
Teresa Do <tdo@classy.org>
2016-05-04 12:59:57 -0700

    Likes resource added

---
+++ src/resources/Likes.js
@@ -0,0 +1,13 @@
+import Resource from '../ClassyResource';
+
+class Likes extends Resource {
+  constructor(Classy) {
+    super(Classy, {
+    	basic: ['delete'],
+      path: '/likes'
+    });
+
+  }
+}
+
+export default Likes;

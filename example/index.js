254496e159061745129a8679cfd9bed8d5aa6241
Matt <mlingner@classy.org>
2016-08-02 12:55:47 -0700

    Add createHardCreditTransfer to transactions

--- example/index.js
+++ example/index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const classy = new Classy({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
+
+  // baseUrl: 'https://dev-gateway.classy-test.org',
   baseUrl: 'https://stagingapi.stayclassy.org',
+
   requestDebug: true
 });
 const app = classy.app();
@@ -19,26 +22,6 @@ app.then((response) => {
   console.log(
     colors.green('✓'),
     colors.gray('/oauth2/auth (app) - ' + response.access_token)
-  );
-
-  classy.organizations.listCampaigns(26820, {
-    token: 'member'
-  }).then(
-    (response) => {
-      const campaigns = response.data.map((data) => data.name + '#' + data.id);
-
-      console.log(
-        colors.green('✓'),
-        colors.gray('List: /organizations/26820/campaigns - ' + campaigns)
-      );
-    },
-
-    (error) => {
-      console.log(
-        colors.red('✗'),
-        colors.gray('List: /organizations/26820/campaigns - ' + error.error)
-      );
-    }
   );
 
   classy.oauth.auth({
@@ -51,78 +34,16 @@ app.then((response) => {
       colors.gray('/oauth2/auth (member) - ' + response.access_token)
     );
 
-    /** Retrieve */
-    classy.me.retrieve(59359).then((response) => {
-
-      console.log(
-        colors.green('✓'),
-        colors.gray('Retrieve: /me - ' + response.first_name + ' ' + response.last_name)
-      );
-    });
-
-    classy.transactions.retrieve(2741758, {
-      token: 'app'
-    }).then((response) => {
-      console.log(
-        colors.green('✓'),
-        colors.gray('Retrieve: /transactions/2741758 - $' + response.total_gross_amount)
-      );
-
-    });
-
-    /** Create */
-    classy.organizations.createCampaign(34, {
-      name: 'Demo Day Campaign',
-      goal: 500,
-      started_at: '2017-10-15 01:00:00',
-      timezone_identifier: 'US/Alaska',
-      type: 'crowdfunding'
+    classy.transactions.createHardCreditTransfer(2733009, {
+      to: {
+        type: 'campaign',
+        id: 56327
+      },
+      note: 'lmao'
     }).then((response) => {
 
-      console.log(
-        colors.green('✓'),
-        colors.gray(
-          'Create: /organizations/34/campaigns - ' + response.name
-          + '#' + response.id
-        )
-      );
     });
 
-    /** Update */
-    classy.campaigns.update(59676, {
-      name: 'Matt\'s Demo Day Campaign'
-    }).then((response) => {
-      console.log(
-        colors.green('✓'),
-        colors.gray(
-          'Update: /campaigns/59676 - ' + response.name + '#' + response.id
-        )
-      );
-    });
-
-    /** List */
-    classy.organizations.listCampaigns(26820, {
-      token: 'app'
-    }).then((response) => {
-      const campaigns = response.data.map((data) => data.name + '#' + data.id);
-
-      console.log(
-        colors.green('✓'),
-        colors.gray('List: /organizations/26820/campaigns - ' + campaigns)
-      );
-    });
-
-    /** Error */
-    classy.organizations.retrieve(900000000).then(
-      (response) => {},
-
-      (error) => {
-        console.log(
-          colors.red('✗'),
-          colors.gray('Retrieve: /organizations/900000000 - ' + error.error)
-        );
-      }
-    );
   });
 
 });

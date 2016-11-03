0d5a22029af34aea6d1f0d1cdb7f71591248ba53
Matt Lingner <mlingner@gmail.com>
2016-11-03 11:15:16 -0700

    Update quote marks for jscs

--- src/ClassyResource.js
+++ src/ClassyResource.js
@@ -100,7 +100,7 @@ export default class ClassyResource {
 
         let requestHeaders = this._classy.headers || _.merge(DEFAULT_REQUEST_HEADERS, spec.headers);
 
-        if (requestMethod !== "GET" && !isAuthRequest) {
+        if (requestMethod !== 'GET' && !isAuthRequest) {
           requestHeaders['Content-Type'] = 'application/json';
         }
 

c550147af7fdb1ce199ab75fbced94cb66826a3a
Jenny Bormacoff <jbormacoff@classy.org>
2019-03-29 11:59:53 -0700

    CL-22 revert createMethod to original

--- src/ClassyResource/createMethod.js
+++ src/ClassyResource/createMethod.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
-import { utils } from "../utils";
+import _ from 'lodash';
+import { utils } from '../utils';
 
 /**
  * [createMethod description]
@@ -15,14 +15,15 @@ export default function createMethod(spec) {
   const PARAM_REGEX = /\{(.*?)\}/g;
 
   /** Get parameterized request URL and method */
-  const specPath = !_.isUndefined(spec.path) ? spec.path : "";
+  const specPath = (!_.isUndefined(spec.path) ? spec.path : '');
   const fullPath = this._urlData.path + specPath;
   const commandPath = utils.makeURLInterpolator(fullPath);
   const urlParams = utils.getRegexMatches(fullPath, PARAM_REGEX);
-  const requestMethod = _.get(spec, "method", "GET").toUpperCase();
+  const requestMethod = _.get(spec, 'method', 'GET').toUpperCase();
 
   /** Return resource method */
   return (...args) => {
+
     /** Extract data from function arguments */
     const _this = this;
     const urlData = this._populateUrlParams(urlParams, args);
@@ -35,15 +36,8 @@ export default function createMethod(spec) {
 
       if (!arg) {
         throw new Error(
-          'Classy: Argument "' +
-            urlParams[i] +
-            '" required, but got: ' +
-            arg +
-            " (on API request to " +
-            requestMethod +
-            " " +
-            commandPath(urlData) +
-            ")"
+          'Classy: Argument "' + urlParams[i] + '" required, but got: ' + arg +
+          ' (on API request to ' + requestMethod + ' ' + commandPath(urlData) + ')'
         );
       }
     }
@@ -51,11 +45,7 @@ export default function createMethod(spec) {
     /** Create full request path with resolved params */
     const resolvedPath = commandPath(urlData);
     const isAuthRequest = utils.isAuthRequest(resolvedPath);
-    const requestPath = this._createFullPath(
-      resolvedPath,
-      isAuthRequest,
-      spec.basePath
-    );
+    const requestPath = this._createFullPath(resolvedPath, isAuthRequest, spec.basePath);
 
     /** Populate form data for authorization requests */
     let form = false;
@@ -66,82 +56,49 @@ export default function createMethod(spec) {
     /**
      * Token stuff & requests need to be synchronous with refresh
      */
-    return this._chooseToken(form, data).then(
-      response => {
-        const DEFAULT_REQUEST_HEADERS = {
-          Accept: "application/json"
-        };
+    return this._chooseToken(form, data).then((response) => {
+      const DEFAULT_REQUEST_HEADERS = {
+        Accept: 'application/json'
+      };
 
-        console.log("this is", this); // eslint-disable-line
+      let requestHeaders = this._classy.headers || _.merge(DEFAULT_REQUEST_HEADERS, spec.headers);
 
-        let requestHeaders =
-          this._classy.headers ||
-          _.merge(DEFAULT_REQUEST_HEADERS, spec.headers);
+      if (requestMethod == 'POST'
+        || requestMethod == 'PUT'
+        || requestMethod == 'PATCH'
+        && !isAuthRequest) {
+        requestHeaders['Content-Type'] = 'application/json';
+      }
 
-        if (
-          requestMethod == "POST" ||
-          requestMethod == "PUT" ||
-          (requestMethod == "PATCH" && !isAuthRequest)
-        ) {
-          requestHeaders["Content-Type"] = "application/json";
-        }
+      if (!response) {
+        return this._makeRequest(requestPath, requestMethod, requestHeaders, form, data);
+      }
 
-        if (!response) {
-          return this._makeRequest(
-            requestPath,
-            requestMethod,
-            requestHeaders,
-            form,
-            data
-          );
-        }
-
-        if (response === "app") {
-          return this._refreshAppToken().then(
-            response => {
-              const appToken = _.get(
-                this._classy.appToken,
-                "access_token",
-                null
-              );
-              delete data.token;
-
-              requestHeaders.Authorization = "Bearer " + appToken;
-
-              return this._makeRequest(
-                requestPath,
-                requestMethod,
-                requestHeaders,
-                form,
-                data
-              );
-            },
-            error => {
-              console.error("App token refresh failed: ", error);
-            }
-          );
-        }
-
-        if (response === "member") {
-          const memberToken = _.get(data, "token.access_token", false);
+      if (response === 'app') {
+        return this._refreshAppToken().then((response) => {
+          const appToken = _.get(this._classy.appToken, 'access_token', null);
           delete data.token;
 
-          requestHeaders.Authorization = "Bearer " + memberToken;
+          requestHeaders.Authorization = 'Bearer ' + appToken;
 
-          return this._makeRequest(
-            requestPath,
-            requestMethod,
-            requestHeaders,
-            form,
-            data
-          );
-        }
-      },
-      error => {
-        throw new Error(
-          "No token defined. Expected memberToken object or `token: 'app'`"
-        );
+          return this._makeRequest(requestPath, requestMethod, requestHeaders, form, data);
+        }, (error) => {
+          console.error('App token refresh failed: ', error);
+        });
       }
-    );
+
+      if (response === 'member') {
+        const memberToken = _.get(data, 'token.access_token', false);
+        delete data.token;
+
+        requestHeaders.Authorization = 'Bearer ' + memberToken;
+
+        return this._makeRequest(requestPath, requestMethod, requestHeaders, form, data);
+      }
+
+    }, (error) => {
+      throw new Error('No token defined. Expected memberToken object or `token: \'app\'`');
+    });
+
   };
 }

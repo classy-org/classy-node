2994fbf73ce031eeee376e4c9c37a1c5493ff84d
Ethan Soch <esoch@classy.org>
2019-11-06 12:30:46 -0800

    NA - Remove redundant error catch

--- src/ClassyResource/_refreshAppToken.js
+++ src/ClassyResource/_refreshAppToken.js
@@ -4,43 +4,27 @@
  * @return {[type]} [description]
  */
 export default function _refreshAppToken() {
-  try {
-    const promise = new Promise((resolve, reject) => {
-      if (!this._classy.appToken) {
-        this._classy.getAppToken().then((response) => {
-          this._classy.setAppToken(response);
+  return new Promise((resolve, reject) => {
+    if (!this._classy.appToken) {
+      this._classy.getAppToken().then((response) => {
+        this._classy.setAppToken(response);
 
-          resolve(response);
-        }, (error) => {
-          /**
-           * Pass the error to _errorLogger if defined
-           */
-          if (this._errorLogger) {
-            this._errorLogger(error, {
-              location: '_refreshAppToken.js',
-              action: '_refreshAppToken() - getAppToken()'
-            });
-          }
+        resolve(response);
+      }, (error) => {
+        /**
+         * Pass the error to _errorLogger if defined
+         */
+        if (this._errorLogger) {
+          this._errorLogger(error, {
+            location: '_refreshAppToken.js',
+            action: '_refreshAppToken() - getAppToken()'
+          });
+        }
 
-          reject(error);
-        });
-      } else {
-        resolve(this._classy.appToken);
-      }
-    });
-
-    return promise;
-  } catch (e) {
-   /**
-     * Pass the error to _errorLogger if defined
-     */
-    if (this._errorLogger) {
-      this._errorLogger(e, {
-        location: '_refreshAppToken.js',
-        action: '_refreshAppToken()'
+        reject(error);
       });
+    } else {
+      resolve(this._classy.appToken);
     }
-
-    throw e;
-  }
+  });
 }

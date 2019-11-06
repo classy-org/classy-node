33ecb9a9ad81b4ca1e8b426aa1a23d5a471d5e3f
Ethan Soch <esoch@classy.org>
2019-11-06 10:58:15 -0800

    NA - Make updates after talking with Joey

--- src/ClassyResource/_makeRequest.js
+++ src/ClassyResource/_makeRequest.js
@@ -10,47 +10,59 @@ import request from 'request';
  * @param  {object} form    Request form (optional)
  * @return {promise}         Promise based on API request
  */
-export default function _makeRequest(path, method, headers, form, data) {
+export default function _makeRequest(path, method, headers = {}, form, data = {}) {
+  let forceQs = null;
+
+  if (data.noLog) {
+    headers['x-no-log'] = true;
+    delete data.noLog;
+  }
+
+  if (_.isObject(data.qs)) {
+    forceQs = data.qs;
+    delete data.qs;
+  }
+
+  // the encoding param defaults to undefined, which means the response will be stringified
+  // we want to preserve the binary encoding for pdfs, so return null in this case
+  const encoding = _.includes(headers.Accept, 'pdf')
+    ? null
+    : undefined;
+
+  const requestParams = {
+    baseUrl: this.baseUrl,
+    uri: path,
+    method: method,
+    headers: headers,
+    rejectUnauthorized: false,
+    form: form,
+    encoding: encoding
+  };
+
+  if (method === 'GET') {
+    requestParams.qs = data;
+  } else {
+    try {
+      requestParams.body = JSON.stringify(data);
+    } catch (e) {
+      /**
+       * Pass the error to _errorLogger if defined
+       */
+      if (this._errorLogger) {
+        this._errorLogger(e, {
+          data,
+          requestParams
+        });
+      }
+    }
+  }
+
+  if (forceQs) {
+    requestParams.qs = _.merge(requestParams.qs || {}, forceQs);
+  }
+
   try {
-    let forceQs = null;
-
-    if (data.noLog) {
-      headers['x-no-log'] = true;
-      delete data.noLog;
-    }
-
-    if (_.isObject(data.qs)) {
-      forceQs = data.qs;
-      delete data.qs;
-    }
-
     const promise = new Promise((resolve, reject) => {
-      // the encoding param defaults to undefined, which means the response will be stringified
-      // we want to preserve the binary encoding for pdfs, so return null in this case
-      const encoding = _.includes(headers.Accept, 'pdf')
-        ? null
-        : undefined;
-
-      const requestParams = {
-        baseUrl: this.baseUrl,
-        uri: path,
-        method: method,
-        headers: headers,
-        rejectUnauthorized: false,
-        form: form,
-        encoding: encoding
-      };
-
-      if (method === 'GET') {
-        requestParams.qs = data;
-      } else {
-        requestParams.body = JSON.stringify(data);
-      }
-
-      if (forceQs) {
-        requestParams.qs = _.merge(requestParams.qs || {}, forceQs);
-      }
-
       request(requestParams, (err, response, body) => {
         if (err || !/^2/.test('' + response.statusCode)) {
           let error;
@@ -76,9 +88,9 @@ export default function _makeRequest(path, method, headers, form, data) {
               location: '_makeRequest.js',
               action: '_makeRequest - request()',
               originalError: err,
-              response: response,
-              body: body,
-              requestParams: requestParams
+              response,
+              body,
+              requestParams
             });
           }
 
@@ -104,13 +116,7 @@ export default function _makeRequest(path, method, headers, form, data) {
         location: '_makeRequest.js',
         action: '_makeRequest()',
         response: response,
-        requestParams: {
-          path: path,
-          method: method,
-          headers: headers,
-          form: form,
-          data: data
-        }
+        requestParams
       });
     }
 
